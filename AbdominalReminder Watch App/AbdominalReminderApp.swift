a0914275d4fa86639caa5086b13e027aa9acaf89
Gabriel Sabadin <gbsabadin@icloud.com>
2025-10-23 12:48:25 -0300

    Initial Commit

---
+++ AbdominalReminder Watch App/AbdominalReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbdominalReminderApp.swift
+//  AbdominalReminder Watch App
+//
+//  Created by Gabriel Belleboni Sabadin on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AbdominalReminder_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AbdominalReminder Watch AppTests/AbdominalReminder_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AbdominalReminder_Watch_AppTests.swift
+//  AbdominalReminder Watch AppTests
+//
+//  Created by Gabriel Belleboni Sabadin on 23/10/25.
+//
+
+import Testing
+@testable import AbdominalReminder_Watch_App
+
+struct AbdominalReminder_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
